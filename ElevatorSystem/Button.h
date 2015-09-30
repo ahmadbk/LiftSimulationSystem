7441d747311564687a2a4b575ed997f60e8994b5
ahmadbk <ahmadbk99@gmail.com>
2015-09-30 03:06:40 +0200

    Button Object Done

---
+++ ElevatorSystem/Button.h
@@ -0,0 +1,71 @@
+#pragma once
+#include <allegro5/allegro.h>
+#include <allegro5/allegro_primitives.h>
+#include <allegro5/allegro_font.h>
+#include <allegro5/allegro_ttf.h>
+
+#include <allegro5/allegro_audio.h>
+#include <allegro5/allegro_acodec.h>
+
+class Button
+{
+private:
+
+	const int height;
+	const int startX;
+	const int startY;
+	const int endX;
+	const int endY;
+	bool buttonPressed = false; //true--> pressed 
+	const int buttonType;		//1 --> elevator button  and 2 --> floor button
+	int FbuttonNumber;
+	int EbuttonNumber;
+	boolean FbuttonDirection;
+
+
+public:
+	Button(int sX, int sY, int eX, int eY, int h, int bT, int n) :startX(sX), startY(sY), endX(eX), endY(eY), height(h), buttonType(bT)
+	{
+		al_draw_rectangle(sX, sY, eX, eY, al_map_rgb(255, 0, 0), h);
+
+		EbuttonNumber = n;
+
+	};
+
+	Button(int sX, int sY, int eX, int eY, int h, int bT, int n, boolean d) :startX(sX), startY(sY), endX(eX), endY(eY), height(h), buttonType(bT)
+	{
+		al_draw_rectangle(sX, sY, eX, eY, al_map_rgb(255, 0, 0), h);
+
+		FbuttonNumber = n;
+		FbuttonDirection = d;
+	};
+
+	~Button(){};
+	boolean CheckButtonPressed(ALLEGRO_EVENT ec)
+	{
+		if (ec.type == ALLEGRO_EVENT_MOUSE_BUTTON_UP) {
+			if (ec.mouse.x >= startX && ec.mouse.x <= endX && ec.mouse.y >= startY && ec.mouse.y < endY)
+				buttonPressed = true;
+			else
+				buttonPressed = false;
+		}
+		return buttonPressed;
+	}
+	int getBType()
+	{
+		return buttonType;
+	}
+	int getBNum()
+	{
+		if (buttonType == 1)
+			return EbuttonNumber;
+		else
+			return FbuttonNumber;
+	}
+	boolean getDirection()
+	{
+		if (buttonType == 2)
+			return FbuttonDirection;
+	}
+};
+
---
+++ ElevatorSystem/Main.cpp
@@ -0,0 +1,264 @@
+#include <stdio.h>
+#include <allegro5/allegro.h>
+#include <allegro5/allegro_primitives.h>
+#include <allegro5/allegro_font.h>
+#include <allegro5/allegro_ttf.h>
+#include"allegro5/allegro_image.h"
+
+#include <allegro5/allegro_audio.h>
+#include <allegro5/allegro_acodec.h>
+
+#include <math.h>
+#include <iostream>
+#include <string>
+
+#include "Button.h"
+
+
+#define PI 3.14159265359
+
+using namespace std;
+
+const float FPS = 45;
+const int SCREEN_W = 640;
+const int SCREEN_H = 700;
+
+enum MYKEYS {
+	KEY_UP, KEY_DOWN, KEY_LEFT, KEY_RIGHT, KEY_SPACE
+};
+
+
+int main(int argc, char **argv)
+
+{
+	srand(time(NULL));
+
+	ALLEGRO_DISPLAY *display = NULL;
+	ALLEGRO_EVENT_QUEUE *event_queue = NULL;
+	ALLEGRO_TIMER *timer = NULL;
+	ALLEGRO_SAMPLE *sample = NULL;
+	ALLEGRO_SAMPLE *SAMMY = NULL;
+	ALLEGRO_SAMPLE *thunder = NULL;
+
+	Button *b1[5];	//1st column - elevator buttons
+	Button *b2[5];	//2nd column - elevator buttons
+	Button *b3[10];	//1st column - floor up buttons
+	Button *b4[10];	//2nd column - floor down buttons
+	b4[9] = NULL;
+	b3[9] = NULL;
+
+	bool key[4] = { false, false, false, false };
+	bool redraw = true;
+	bool doexit = false;
+
+	if (!al_init()) {
+		fprintf(stderr, "failed to initialize allegro!\n");
+		return -1;
+	}
+
+	if (!al_install_keyboard()) {
+		fprintf(stderr, "failed to initialize the keyboard!\n");
+		return -1;
+	}
+
+	if (!al_init_primitives_addon())
+	{
+		fprintf(stderr, "failed to initialize primitives addon!\n");
+		return -1;
+	}
+
+	timer = al_create_timer(1.0 / FPS);
+	if (!timer) {
+		fprintf(stderr, "failed to create timer!\n");
+		return -1;
+	}
+
+	display = al_create_display(SCREEN_W, SCREEN_H);
+
+	if (!display) {
+		fprintf(stderr, "failed to create display!\n");
+		al_destroy_timer(timer);
+		return -1;
+	}
+
+	if (!al_install_mouse()) {
+		fprintf(stderr, "failed to initialize the mouse!\n");
+		return -1;
+	}
+
+	al_init_font_addon();
+	al_init_ttf_addon();
+
+	ALLEGRO_FONT *font = al_load_ttf_font("CFNuclearWar-Regular.ttf", 32, 0);
+	ALLEGRO_FONT *font1 = al_load_ttf_font("CFNuclearWar-Regular.ttf", 72, 0);
+	ALLEGRO_FONT *font2 = al_load_ttf_font("CFNuclearWar-Regular.ttf", 30, 0);
+	ALLEGRO_FONT *font3 = al_load_ttf_font("CFNuclearWar-Regular.ttf", 10, 0);
+
+	if (!font){
+		fprintf(stderr, "Could not load font.\n");
+		return -1;
+	}
+
+	if (!al_init_image_addon()) {
+		fprintf(stderr, "Failed to initialize image addon!\n");
+	}
+
+	al_set_target_bitmap(al_get_backbuffer(display));
+
+	event_queue = al_create_event_queue();
+	if (!event_queue) {
+		fprintf(stderr, "failed to create event_queue!\n");
+		al_destroy_display(display);
+		al_destroy_timer(timer);
+		return -1;
+	}
+
+	al_register_event_source(event_queue, al_get_display_event_source(display));
+	al_register_event_source(event_queue, al_get_timer_event_source(timer));
+	al_register_event_source(event_queue, al_get_keyboard_event_source());
+	al_register_event_source(event_queue, al_get_mouse_event_source());
+	al_clear_to_color(al_map_rgb(0, 0, 0));
+
+	al_flip_display();
+	al_start_timer(timer);
+
+
+	bool menu = true;
+
+	while (menu)
+	{
+		int numOfButtons = 1;
+		int startx = 20;
+		int starty = 20;
+		int ButtonType;
+
+		al_draw_text(font2, al_map_rgb(255, 0, 40), startx, starty, ALLEGRO_ALIGN_LEFT, "INSIDE ELEVATOR");
+		al_draw_text(font2, al_map_rgb(255, 0, 40), startx + 320, starty, ALLEGRO_ALIGN_LEFT, "OUTSIDE ELEVATOR");
+		al_draw_line(320, 0, 320, 700, al_map_rgb(255, 0, 40), 10);
+
+
+		//-----------------------------------------------------------------
+
+		//1st column of blocks outside the elevator
+		int sY = 70;
+		int eY = 100;
+		int height = 2;
+		ButtonType = 1;		//Elevator Button
+		for (int i = 0; i < 5; i++)
+		{
+			b1[i] = new Button(70, sY, 100, eY, height, ButtonType, (i + 1));
+			al_draw_text(font3, al_map_rgb(0, 0, 255), 80, sY + 10, ALLEGRO_ALIGN_LEFT, "1");
+			sY += 50;
+			eY += 50;
+		}
+
+		//------------------------------------------------------------------
+
+		//2nd Column of blocks outside lift
+		sY = 70;
+		eY = 100;
+		for (int i = 0; i < 5; i++)
+		{
+			b2[i] = new Button(150, sY, 180, eY, height, ButtonType, (i + 1 + 5));
+			al_draw_text(font3, al_map_rgb(0, 0, 255), 160, sY + 10, ALLEGRO_ALIGN_LEFT, "2");
+			sY += 50;
+			eY += 50;
+		}
+
+		//------------------------------------------------------------------
+
+		//Draw Lines in between floors
+		sY = 70;
+		eY = 70;
+		for (int i = 0; i < 11; i++)
+		{
+			al_draw_line(550, sY, 640, eY, al_map_rgb(255, 0, 40), 2);
+			sY += 60;
+			eY += 60;
+		}
+
+		//------------------------------------------------------------------
+
+		//Floor Buttons
+		sY = 100;
+		eY = 120;
+		ButtonType = 2;		//Floor Button
+		for (int i = 0; i < 10; i++)
+		{
+			if (i != 0)
+			{
+				b3[i] = new Button(570, sY, 590, eY, height, ButtonType, (10 - i), true);
+				al_draw_text(font3, al_map_rgb(0, 0, 255), 576, sY + 4, ALLEGRO_ALIGN_LEFT, "U");
+			}
+			if (i != 9)
+			{
+				b4[i] = new Button(610, sY, 630, eY, height, ButtonType, (10 - i), false);
+				al_draw_text(font3, al_map_rgb(0, 0, 255), 616, sY + 4, ALLEGRO_ALIGN_LEFT, "D");
+			}
+			sY += 60;
+			eY += 60;
+		}
+
+		//------------------------------------------------------------------
+
+		al_flip_display();
+		ALLEGRO_EVENT ec;
+		al_wait_for_event(event_queue, &ec);
+
+		if (ec.type == ALLEGRO_EVENT_MOUSE_BUTTON_UP)
+		{
+			for (int i = 0; i < 5; i++)
+			{
+				boolean fff1 = false;
+				boolean fff2 = false;
+				fff1 = b1[i]->CheckButtonPressed(ec);
+				fff2 = b2[i]->CheckButtonPressed(ec);
+				if (fff1)
+					printf("%d\n", b1[i]->getBNum());
+				if (fff2)
+					printf("%d\n", b2[i]->getBNum());
+			}
+
+			for (int i = 0; i < 10; i++)
+			{
+				boolean fff1 = false;
+				boolean fff2 = false;
+				if (i != 0)
+					fff1 = b3[i]->CheckButtonPressed(ec);
+				if (i != 9)
+					fff2 = b4[i]->CheckButtonPressed(ec);
+				if (fff1)
+					printf("(%d,%d)\n", b3[i]->getBNum(), b3[i]->getDirection());
+				if (fff2)
+					printf("(%d,%d)\n", b4[i]->getBNum(), b4[i]->getDirection());
+			}
+
+		}
+
+		if (ec.type == ALLEGRO_EVENT_DISPLAY_CLOSE)
+		{
+			exit(1);
+		}
+
+	}
+
+	while (!doexit)
+	{
+		ALLEGRO_EVENT ev;
+		al_wait_for_event(event_queue, &ev);
+
+		if (redraw && al_is_event_queue_empty(event_queue))
+		{
+			al_flip_display();
+		}
+
+	}
+
+	al_destroy_font(font);
+	al_destroy_timer(timer);
+	al_destroy_display(display);
+	al_destroy_event_queue(event_queue);
+	al_destroy_sample(sample);
+
+	return 0;
+}
